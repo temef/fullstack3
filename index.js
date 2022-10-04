13439425a9d2924b9411752a8031b03c2a85d911
Teemu Romo <teemu.romo@aalto.fi>
2022-10-04 16:35:50 +0300

    3kappale

---
+++ index.js
@@ -0,0 +1,106 @@
+const { response } = require("express");
+const express = require("express");
+const morgan = require("morgan")
+const cors = require("cors")
+const app = express();
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-1234567",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  },
+];
+
+
+app.use(cors())
+app.use(express.json())
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :body'))
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const date = new Date()
+  res.send(
+    `<div>
+    <p>Phonebook has info for ${persons.length} people </p>
+    <p>${date}</p>`
+    )
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = persons.find(person => person.id === id)
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  // console.log(req)
+  // console.log(body)
+
+  if (!body.name) {
+    return res.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  if (!body.number) {
+    return res.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+  if (persons.find(person => person['name'] === body.name)) {
+    return res.status(400).json({ 
+      error: `${body.name} is already in the phonebook` 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor((Math.random()*1000))
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
